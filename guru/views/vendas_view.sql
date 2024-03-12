dc1328c18fae6c708ddc55223da72163bacb1e8f
Mateus Cardoso <71328544+mateusncardoso@users.noreply.github.com>
2024-03-12 14:41:11 -0300

    Update vendas_view.sql

--- guru/views/vendas_view.sql
+++ guru/views/vendas_view.sql
@@ -3,4 +3,4 @@ OPTIONS(
 description="the latest representation of each cart"
 )
 as
-select * from `eu-medico-residente.guru.vendas` limit 10
+select * from `eu-medico-residente.guru.vendas` limit 15
