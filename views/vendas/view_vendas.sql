09bd090c392d54c9afb3896652a09f324df5adf0
Mateus Cardoso <71328544+mateusncardoso@users.noreply.github.com>
2024-03-12 10:06:00 -0300

    Delete views/vendas directory

--- views/vendas/view_vendas.sql
+++
@@ -1,6 +0,0 @@
-create or replace view `<project_id>.guru.view_vendas_teste`
-OPTIONS(
-description="the latest representation of each cart"
-)
-as
-select * from `<project_id>.guru.vendas` limit 1000
